508ae3127af33d1b412c42852ab54dea76b620a1
zenkichen <zenkichen@gmail.com>
2017-11-14 16:22:59 +0800

    init
    create

---
+++ float2char/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace float2char
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(250, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "float";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "byte[]";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(248, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "High";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(359, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Low";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "etc.  0.0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 12);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "etc.  0.0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(205, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(39, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(130, 104);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(39, 23);
+            this.button4.TabIndex = 21;
+            this.button4.Text = ">";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(509, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 12);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Low";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(248, 128);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "High";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(305, 89);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 12);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "byte[]";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(52, 89);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 12);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "double";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(250, 103);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(282, 21);
+            this.textBox9.TabIndex = 13;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(24, 104);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 21);
+            this.textBox10.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 286);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "浮点数内存转换";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ float2char/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace float2char
+{
+    public partial class Form1 : Form
+    {
+        FDOUBLE fdouble;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(chkVildFloat(textBox1.Text.Trim()))
+            {
+                float data = float.Parse(textBox1.Text.Trim());
+                fdouble.fVal0 = data;
+                textBox2.Text = fdouble.cVal0.ToString("X2"); textBox2.Text += " ";
+                textBox2.Text += fdouble.cVal1.ToString("X2"); textBox2.Text += " ";
+                textBox2.Text += fdouble.cVal2.ToString("X2"); textBox2.Text += " ";
+                textBox2.Text += fdouble.cVal3.ToString("X2");
+            }
+            else
+            {
+                MessageBox.Show("数据格式错误");
+            }
+        }
+        public static string HexToString(byte[] bytes)
+        {
+            string returnStr = "";
+            if (bytes != null)
+            {
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    returnStr += bytes[i].ToString("X2");
+                }
+            }
+            return returnStr;
+        }
+
+        public static String byte2HexStr(byte[] b)
+        {
+            String stmp = "";
+            stmp = HexToString(b);
+            return stmp.ToString().ToLower().Trim();
+        }
+
+        private bool chkVildFloat(string str)
+        {
+            //先判断是否有"."号
+            if (str.Contains("."))
+            {
+                try
+                {
+                    //是否能转为浮点数
+                    float temp = float.Parse(str);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool chkVildDouble(string str)
+        {
+            //先判断是否有"."号
+            if (str.Contains("."))
+            {
+                try
+                {
+                    //是否能转为浮点数
+                    double temp = double.Parse(str);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        [StructLayout(LayoutKind.Explicit, Size = 8)]
+        public struct FDOUBLE
+        {
+            [FieldOffset(0)]
+            public double dbVal;
+            [FieldOffset(0)]
+            public float fVal0;
+            [FieldOffset(4)]
+            public float fVal1;
+            [FieldOffset(0)]
+            public int nVal0;
+            [FieldOffset(4)]
+            public int nVal1;
+            [FieldOffset(0)]
+            public short nsVal0;
+            [FieldOffset(2)]
+            public short nsVal1;
+            [FieldOffset(4)]
+            public short nsVal2;
+            [FieldOffset(6)]
+            public short nsVal3;
+            [FieldOffset(0)]
+            public byte cVal0;
+            [FieldOffset(1)]
+            public byte cVal1;
+            [FieldOffset(2)]
+            public byte cVal2;
+            [FieldOffset(3)]
+            public byte cVal3;
+            [FieldOffset(4)]
+            public byte cVal4;
+            [FieldOffset(5)]
+            public byte cVal5;
+            [FieldOffset(6)]
+            public byte cVal6;
+            [FieldOffset(7)]
+            public byte cVal7;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str = textBox2.Text.Trim();
+            string[] strArray = str.Split(' ');
+            fdouble.cVal0 = Convert.ToByte(strArray[0], 16);
+            fdouble.cVal1 = Convert.ToByte(strArray[1], 16);
+            fdouble.cVal2 = Convert.ToByte(strArray[2], 16);
+            fdouble.cVal3 = Convert.ToByte(strArray[3], 16);
+            textBox1.Text = fdouble.fVal0.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (chkVildDouble(textBox10.Text.Trim()))
+            {
+                double data = double.Parse(textBox10.Text.Trim());
+                fdouble.dbVal = data;
+                textBox9.Text = fdouble.cVal0.ToString("X2"); textBox9.Text += " ";
+                textBox9.Text += fdouble.cVal1.ToString("X2"); textBox9.Text += " ";
+                textBox9.Text += fdouble.cVal2.ToString("X2"); textBox9.Text += " ";
+                textBox9.Text += fdouble.cVal3.ToString("X2"); textBox9.Text += " ";
+                textBox9.Text += fdouble.cVal4.ToString("X2"); textBox9.Text += " ";
+                textBox9.Text += fdouble.cVal5.ToString("X2"); textBox9.Text += " ";
+                textBox9.Text += fdouble.cVal6.ToString("X2"); textBox9.Text += " ";
+                textBox9.Text += fdouble.cVal7.ToString("X2");
+            }
+            else
+            {
+                MessageBox.Show("数据格式错误");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = textBox9.Text.Trim();
+            string[] strArray = str.Split(' ');
+            fdouble.cVal0 = Convert.ToByte(strArray[0], 16);
+            fdouble.cVal1 = Convert.ToByte(strArray[1], 16);
+            fdouble.cVal2 = Convert.ToByte(strArray[2], 16);
+            fdouble.cVal3 = Convert.ToByte(strArray[3], 16);
+            fdouble.cVal4 = Convert.ToByte(strArray[4], 16);
+            fdouble.cVal5 = Convert.ToByte(strArray[5], 16);
+            fdouble.cVal6 = Convert.ToByte(strArray[6], 16);
+            fdouble.cVal7 = Convert.ToByte(strArray[7], 16);
+            textBox10.Text = fdouble.dbVal.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
